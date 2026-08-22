6df8c778bddd1ce142e8d3f51a8c01ec4a9faa3a
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-22 09:08:30 +0100

    build: defining user types and access

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Creating users identified by their email address."""
+
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        # hash the password before saving the user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
