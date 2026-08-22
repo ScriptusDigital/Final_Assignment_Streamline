96ab3e8cddcccb9c1693a249892ac0c0792f4e30
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-22 10:24:20 +0100

    build: test script writing for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,4 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+class UserManagerTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email='test@example.com',
+            password='testpassword'
+        )
+        self.assertEqual(user.email, 'viewer@example.com')
+        self.assertEqual(user.role, User.Role.VIEWER)
+        self.assertTrue(user.check_password('testpassword'))
+        self.assertFalse(user.is_staff),
+        self.assertFalse(user.is_superuser)
