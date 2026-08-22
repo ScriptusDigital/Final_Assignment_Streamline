6a106eac7026cab32ad4999f629a7078f937c81c
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-22 11:30:31 +0100

    build: accounts section views.py wiring

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+from .serializers import RegistrationSerializer, UserSerializer
+
