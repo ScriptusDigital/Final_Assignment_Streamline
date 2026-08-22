5b2e6c3cd11bb1c9250840438e352eb5063b14f6
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-22 12:44:22 +0100

    build: csrf token in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,20 @@ from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 
 from .serializers import LoginSerializer, LoginSerializer, RegistrationSerializer, UserSerializer, UserSerializer
 
+""" csrf_protect decorator ensures that the view is protected against Cross-Site Request Forgery (CSRF) attacks."""
+@method_decorator(ensure_csrf_cookie, name='dispatch')
+class CsrfView(APIView):
+    """View to provide a CSRF token."""
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        """Return a CSRF token in the response."""
+        get_token(request)
+        return Response({"detail": "CSRF token provided."}, status=status.HTTP_200_OK)
+
+@method_decorator(csrf_protect, name='dispatch')
+
 class RegisterView(APIView):
     """Create a new Viewer user account."""
     authentication_classes = []
@@ -24,6 +38,7 @@ class RegisterView(APIView):
 
         if serializer.is_valid():
             user = serializer.save()
+            django_login(request, user)
 
             return Response(
                 UserSerializer(user).data,
