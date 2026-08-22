2edfae17acccb813506c610bd7263114553eb358
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-22 12:20:54 +0100

    build: url added into accounts url for login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 
 from django.urls import path
-from .views import RegisterView
+from .views import LoginView, RegisterView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path("auth/register/",
-         RegisterView.as_view(),
-         name="register"),
+    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", LoginView.as_view(), name="login"),
 ]
