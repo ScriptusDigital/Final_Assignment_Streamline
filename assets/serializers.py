8c76b3b12d7fc644119fcf3dbc55ab81dbdcae02
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 11:50:26 +0100

    build: adding serializer python logic

--- assets/serializers.py
+++ assets/serializers.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from .models import Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    asset_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
