d598e73c4bea2e42e634eb5f67a408fb91ff090e
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 13:38:13 +0100

    build: logic for urls.py routes

--- assets/urls.py
+++ assets/urls.py
@@ -1,1 +1,15 @@
 """API routes for the assets app."""
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import  CollectionViewSet, TagViewSet
+
+router = DefaultRouter()
+
+router.register(r"tags", TagViewSet, basename="tag")
+router.register(r"collections", CollectionViewSet, basename="collection")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
