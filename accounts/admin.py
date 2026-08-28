9dd05d5cff5b117c83f612df1546a166db8da14b
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 11:02:35 +0100

    fix: admin user definition in accounts admin py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,22 +3,93 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+
 @admin.register(User)
 class StreamlineUserAdmin(UserAdmin):
     model = User
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_active")
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Personal Info", {"fields": ("first_name", "last_name")}),
-        ("Permissions", {"fields": ("is_staff", "is_active")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
+
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+        "is_active",
     )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
-        }),
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_superuser",
+        "is_active",
     )
-    search_fields = ("email",)
     ordering = ("email",)
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        (
+            "Streamline access",
+            {
+                "fields": ("role",)
+            },
+        ),
+        (
+            "Django permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "role",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
