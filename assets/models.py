f5f46179b6d7be95e042afa79199f1e369fb7914
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 09:21:44 +0100

    chore: setting up assets section python

---
+++ assets/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+"""Database models for Streamline's image catalogue"""
+
+from ___future__ import annotations
+
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
--- streamline/settings.py
+++ streamline/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "accounts",
+    "assets",
 ]
 
 MIDDLEWARE = [
