d9840523b1109ac1b392d677b338a0f53781bae6
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 12:09:35 +0100

    fix: removing code entered in wrong serializer

--- assets/serializers.py
+++ assets/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 from .models import Tag
 
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from.models import Collection, Tag
+
+class UserSummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'display_name', 'role')
+    def get_display_name(self, obj):
+        return obj.get_display_name() or obj.email  
 
 class TagSerializer(serializers.ModelSerializer):
     asset_count = serializers.IntegerField(read_only=True)
