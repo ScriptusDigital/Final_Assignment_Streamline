7b0911f0009b9944da1fc693f857d4a291dfb8c1
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 13:24:35 +0100

    build: creating permission class for asset model

--- assets/permissions.py
+++ assets/permissions.py
@@ -1,4 +1,19 @@
-""" ROles and permissions for the assets app. """
+""" Roles and permissions for the assets app. """
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 from .services import workflow_service
+
+class AssetPermission(BasePermission):
+    """ Custom permission class for Asset model. """
+    message = "You do not have access to this asset."
+
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        if view.action == "create":
+            return workflow_service.user_role(
+                request.user
+            ) in ("editor", "admin")
+
+        return True
