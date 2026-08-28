5421f58f3ca03f907f2c8cb719a803ea381d1deb
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 13:36:10 +0100

    build: assets view.py logic

--- assets/views.py
+++ assets/views.py
@@ -8,3 +8,23 @@ from .models import Collection, Tag
 from .permissions import TaxonomyPermission
 from .serializers import CollectionSerializer, TagSerializer
 
+class TagViewSet(viewsets.ModelViewSet):
+    """ ViewSet for managing tags. """
+    serializer_class = TagSerializer
+    permission_classes = [IsAuthenticated, TaxonomyPermission]
+
+    pagination_class = None  
+
+    def get_queryset(self):
+        """ Return all tags with the count of associated assets. """
+        return Tag.objects.annotate(asset_count=Count("assets", filter=Q(assets=True))).order_by("name")
+
+class CollectionViewSet(viewsets.ModelViewSet):
+    """ ViewSet for managing collections. """
+    serializer_class = CollectionSerializer
+    permission_classes = [IsAuthenticated, TaxonomyPermission]
+    pagination_class = None
+
+    def get_queryset(self):
+        """ Return all collections with the count of associated assets. """
+        return Collection.objects.select_related("created_by").annotate(asset_count=Count("assets",distinct=True)).order_by("name")
