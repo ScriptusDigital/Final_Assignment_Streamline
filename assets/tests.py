a1a81f6d1d90179e5dafd357db65a2b4327acc6a
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 11:09:01 +0100

    chore: import and setup for asset db section tests

--- assets/tests.py
+++ assets/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+""" Tests for asset models """
 
-# Create your tests here.
+
+from datetime import timedelta
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Asset, Tag
