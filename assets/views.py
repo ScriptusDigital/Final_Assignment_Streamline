8357789ea8ec82ea6faa72d968ac36e991074f08
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-28 13:33:45 +0100

    build: views logic for assets

--- assets/views.py
+++ assets/views.py
@@ -1,2 +1,10 @@
 """ Rest API views for the assets app. """
 
+from django.db.models import Count
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Collection, Tag
+from .permissions import TaxonomyPermission
+from .serializers import CollectionSerializer, TagSerializer
+
