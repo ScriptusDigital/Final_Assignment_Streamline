856adf5386f671f6192972fb35ea42f863420f7d
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-08-29 07:34:52 +0100

    build: defining cloudinary imaging parameters

--- assets/services/cloudinary_service.py
+++ assets/services/cloudinary_service.py
@@ -2,3 +2,13 @@
 from __future__ import annotations
 from PIL import Image, UnidentifiedImageError
 
+MAX_UPLOAD_BYTES = 10 * 1024 * 1024
+
+ALLOWED_PILLOW_FORMATS = {
+    "JPEG": "JPEG",
+    "PNG": "PNG",
+    "GIF": "GIF",
+    "BMP": "BMP",
+    "TIFF": "TIFF",
+    "WEBP": "WEBP",
+}
