d0bff9d129ad9ebe1adb3a2e1ae6f7c20e60fe15
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-09-04 12:18:51 +0100

    build: placeholder page build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,3 +6,13 @@ import {
 
 import { ProtectedRoute } from './components/routing/ProtectedRoute'
 import { RoleRoute } from './components/routing/RoleRoute'
+
+function PlaceholderPage ({ title, description }) {
+  return (
+    <main>
+      <p>Streamline</p>
+      <h1>{title}</h1>
+      <p>{description}</p>  
+    </main>
+  )
+}
