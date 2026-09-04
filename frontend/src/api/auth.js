42c9aa5c3d1e64585392ec0da20e1550e76fc737
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-09-04 06:39:39 +0100

    build: auth.js js logic

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,1 +1,16 @@
 import { apiRequest } from './client';
+
+export function initialiseCsrf() {
+  return apiRequest('/auth/csrf/')
+}
+
+export function getCurrentUser() {
+  return apiRequest('/auth/me/')
+}
+
+export function loginUser(credentials) {
+  return apiRequest('/auth/login/', {
+    method: 'POST',
+    body: credentials,
+  })
+}
